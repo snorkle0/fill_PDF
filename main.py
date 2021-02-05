0b0c919fbd4b4ea623262212435b32c1d6f5ac37
Igor Paszkiewicz <igor.paszkiewicz@ingka.ikea.com>
2021-02-05 15:50:10 +0100

    ADD: parse JSON as column headers in input

--- main.py
+++ main.py
@@ -5,28 +5,35 @@ import csv
 import json
 
 
+
+
+
 def read_input():
     path = os.getcwd() + '\Input'
-
-
     files = os.listdir(path)
+    cols = read_json()
 
-    all_df = pd.DataFrame()
+    all_df = pd.DataFrame(columns=cols)
 
     for file in files:
         file_path = f'{path}\{file}'
         df = pd.read_excel(file_path).iloc[1:]
-        # df.iloc[1:]
-        all_df.append(df)
-        print(df)
+        all_df = all_df.append(df)
 
-    # print(all_df)
+    all_df.to_csv('output.csv')
+    print(all_df)
 
 def read_json():
     json_path = os.getcwd() + '\Template\inputHeaders.json'
 
+    with open(json_path) as json_file:
+        data = json.load(json_file)
+        # print(data['headers'])
+        return data['headers']
+
 
 read_input()
+
 
 def list_PDF_fields():
     path = os.getcwd() + '\Template'
@@ -49,16 +56,10 @@ def list_PDF_fields():
                 pass
             return
 
+
 def write_csv(fields):
     fname = 'test.csv'
     with open(fname,'wb') as f:
         w = csv.writer(f)
         w.writerow(fields.keys())
         w.writerow(dict.values())
-
-
-# list_PDF_fields()
-# testDict = list_PDF_fields()
-#
-#
-# write_csv(testDict)
